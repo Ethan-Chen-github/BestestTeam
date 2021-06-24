1bc3cb3fd576bb6be7b427fa704fc40d3b219b6d
Ethan Chen <ethanchen100598@gmail.com>
2021-06-24 12:15:32 -0400

    add lex

--- src/components/AWSLEX.js
+++ src/components/AWSLEX.js
@@ -1,37 +1,62 @@
 import logo from "./bp.png";
 import React from "react";
 import { useState } from "react";
-import { Divider, Button,Input} from "antd";
+import { Divider, Button, Input, Form, Checkbox } from "antd";
 import { AudioTwoTone } from "@ant-design/icons";
 import LexChat from "react-lex";
 
-
 export default function App() {
-  const [showVoice, setShowVoice] = useState(false);
-  const [botName, setBotName] = useState('')
-  const [poolId, setPoolId] = useState('')
- 
-  function Voice() {
-    setShowVoice(true);
-  }
+  const [botName, setBotName] = useState("");
+  const [poolId, setPoolId] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const onFinish = (values) => {
+    console.log(values);
+    setBotName(values["botName"]);
+    setPoolId(values["poolID"]);
+    setShowChat(true);
+  };
 
   return (
     <div className="App">
-      <Input placeholder="botName" />
-      <Input placeholder="IdentityPoolId" />
-      <Button type="primary">Primary Button</Button>
+      <br />
+      <br />
+      <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish}>
+        <Form.Item
+          label="botName"
+          name="botName"
+          rules={[{ required: true, message: "Please input your botName!" }]}
+        >
+          <Input />
+        </Form.Item>
 
+        <Form.Item
+          label="poolID"
+          name="poolID"
+          rules={[{ required: true, message: "Please input your poolID!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <LexChat
-        id = 'lexBot'
-        botName= {botName}
-        IdentityPoolId= {poolId}
-        placeholder="Placeholder text"
-        style={{ position: "absolute" }}
-        backgroundColor="#FFFFFF"
-        height="430px"
-        headerText="Chat with our awesome bot"
-      />
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Start Lex Bot
+          </Button>
+        </Form.Item>
+      </Form>
+      {showChat ? (
+        <LexChat
+          botName= {botName}
+          IdentityPoolId= {poolId}
+          placeholder="Placeholder text"
+          style={{ position: "absolute" }}
+          backgroundColor="#FFFFFF"
+          height="500px"
+          headerText="Chat with our awesome bot"
+        />
+      ) : (
+        <></>
+      )}
 
       <br />
       <br />
