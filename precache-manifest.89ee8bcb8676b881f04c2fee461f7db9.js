e94f88d08885e583b8b7c2b286619537a95b3b67
Ethan Chen <ethanchen100598@gmail.com>
2021-07-14 19:49:29 -0400

    Updates

---
+++ precache-manifest.89ee8bcb8676b881f04c2fee461f7db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbeb917c24419ad31b5bc7745850a8a",
+    "url": "/react-ant-init/index.html"
+  },
+  {
+    "revision": "e6c62b4e261a7b3e2c40",
+    "url": "/react-ant-init/static/css/main.d7dcaba2.chunk.css"
+  },
+  {
+    "revision": "bc06432960bb03ac4b41",
+    "url": "/react-ant-init/static/js/2.e1a32b21.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-ant-init/static/js/2.e1a32b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c62b4e261a7b3e2c40",
+    "url": "/react-ant-init/static/js/main.41eeca30.chunk.js"
+  },
+  {
+    "revision": "1521d445284f98934a51",
+    "url": "/react-ant-init/static/js/runtime-main.09da17b8.js"
+  }
+]);
