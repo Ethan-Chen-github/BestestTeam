7076c0495ea350147616fa2e5900be8201c1f659
Ethan Chen <ethanchen100598@gmail.com>
2021-07-23 14:57:19 -0400

    ian

--- src/DownloadServiceHistory.js
+++ src/DownloadServiceHistory.js
@@ -17,14 +17,28 @@ const DownloadServiceHistory = () => {
         alignContent: "center",
         verticalAlign: "middle",
         height: "100%",
+        // width: "100%",
+        textAlign: "center",
+        marginLeft: "12.5%",
+        marginRight: "12.5%",
       }}
     >
+      Service History is a way to store observations about a particular Wind
+      Turbine. The format of this download will be a CSV File. Inside the CSV,
+      there is the turbine number, date of the observation, and the observation
+      itself.
       <Button
         type="primary"
         shape="round"
         icon={<DownloadOutlined />}
         size="large"
         onClick={downloadFile}
+        style={{
+          width: "50%",
+          marginLeft: "25%",
+          marginTop: "5%",
+          marginRight: "25%",
+        }}
       >
         Download
       </Button>
